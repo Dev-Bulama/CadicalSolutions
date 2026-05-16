bdb107a39b4b35d6367adeb3877d26714ee9dcff
Mark Ogujiuba <markogujiuba@gmail.com>
2026-05-16 16:41:26 +0100

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,53 @@
-export default function Page() {
+
+import FeaturedProduct from "@/components/featured-products";
+import Compliance from "@/components/home/Compliance";
+import Consent from "@/components/home/Consent";
+import Coverage from "@/components/home/Coverage";
+import CTA from "@/components/home/cta";
+import Hero from "@/components/home/hero";
+import Portals from "@/components/home/portals";
+import Process from "@/components/home/process";
+import Services from "@/components/home/services";
+import Why from "@/components/home/why";
+
+
+
+
+export default function Home() {
+
+
+
   return (
-    <div>
-      The page is down call best if you want it up again
-    </div>
-  )
+    <main className="font-sans text-[#1a2332] bg-white overflow-x-hidden">
+
+     <Hero />
+
+      <Portals />
+
+      {/* WHY */}
+      <Why />
+
+      {/* SERVICES */}
+      <Services />
+
+      {/* Products */}
+      <FeaturedProduct />
+
+      {/* PROCESS */}
+      <Process />
+
+      {/* CTA */}
+      <CTA />
+
+      {/* Compliance */}
+      <Compliance />
+
+      {/* Consent */}
+      <Consent />
+
+      {/* Coverage */}
+      <Coverage />
+
+    </main>
+  );
 }
-// import FeaturedProduct from "@/components/featured-products";
-// import Compliance from "@/components/home/Compliance";
-// import Consent from "@/components/home/Consent";
-// import Coverage from "@/components/home/Coverage";
-// import CTA from "@/components/home/cta";
-// import Hero from "@/components/home/hero";
-// import Portals from "@/components/home/portals";
-// import Process from "@/components/home/process";
-// import Services from "@/components/home/services";
-// import Why from "@/components/home/why";
-
-
-
-
-// export default function Home() {
-
-
-
-//   return (
-//     <main className="font-sans text-[#1a2332] bg-white overflow-x-hidden">
-
-//      <Hero />
-
-//       <Portals />
-
-//       {/* WHY */}
-//       <Why />
-
-//       {/* SERVICES */}
-//       <Services />
-
-//       {/* Products */}
-//       <FeaturedProduct />
-
-//       {/* PROCESS */}
-//       <Process />
-
-//       {/* CTA */}
-//       <CTA />
-
-//       {/* Compliance */}
-//       <Compliance />
-
-//       {/* Consent */}
-//       <Consent />
-
-//       {/* Coverage */}
-//       <Coverage />
-
-//     </main>
-//   );
-// }
